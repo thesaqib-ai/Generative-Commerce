0d43780c1f1b679f2c643e38cd09366a6df76f8d
thesaqib-ai <md.saqib370@gmail.com>
2025-04-24 08:57:49 +0500

    Initial Commit

--- middleware/admin.js
+++
@@ -1,9 +0,0 @@
-async function checkAdmin(req,res,next){
-    if(req.user.isAdmin){
-        next();
-        return;
-    }
-    res.status(403).send("User don't have access to the resource!")
-}
-
-module.exports = checkAdmin;
